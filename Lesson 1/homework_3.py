7deb5dbb67b2b2acbb7a39d5de9f112b767d60c0
Mosinmiloluwa Moronkeji <sinmimoronkeji@Mosinmiloluwas-MacBook-Air.local>
2025-08-16 08:04:04 -0500

    hi

--- Lesson 1/homework_3.py
+++ Lesson 1/homework_3.py
@@ -1,4 +1,18 @@
 from tkinter import *
+import random
+
+def check_awn():
+    user = int(guess_entry.get())
+    if user == rando:
+        high_low.config(text="Correct!")
+    elif user < rando:
+        high_low.config(text="Too low!")
+    elif user > rando:
+        high_low.config(text="Too high!")
+
+
+
+rando=random.randint(1,50)
 
 window = Tk()
 window.title("Product Guessing Game")
@@ -10,19 +24,22 @@ heading = Label(window, text="Product Guessing Game", font=("Helvetica", 18, "bo
 heading.place(x=10,y=10)
 
 tries_label = Label(window, text="You have 5 tries left", font=("Helvetica", 12), bg="white")
-tries_label.place(x=10,y=30)
+tries_label.place(x=10,y=40)
 
-guess_frame = Frame(window, bg="white")
-guess_frame.place(x=10,y=80)
+#guess_frame = Frame(window, bg="white",)
+#guess_frame.place(x=10,y=80)
 
-guess_label = Label(guess_frame, text="Your Guess:", font=("Helvetica", 12), bg="white")
-guess_label.place(x=20,y=100)
+guess_label = Label(window, text="Your Guess:", font=("Helvetica", 12), bg="white")
+guess_label.place(x=10,y=103)
 
-guess_entry = Entry(guess_frame, width=25)
-guess_entry.place(x=40,y=100)
+guess_entry = Entry(window, width=25)
+guess_entry.place(x=80,y=100)
 
-check_button = Button(guess_frame, text="Check", bg="red", fg="red", font=("Helvetica", 10, "bold"))
-check_button.place(x=80,y=100)
+check_button = Button(window, text="Check", bg="red", fg="red", font=("Helvetica", 10, "bold"))
+check_button.place(x=330,y=103)
 
+high_low = Label(window, text="", font=("Helvetica", 12), bg="light gray",fg="black")
+high_low.place(x=205,y=153)
 
 window.mainloop()
+
