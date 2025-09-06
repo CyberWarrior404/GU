5f79368bff607d3954d1aed8f80634dce7285d01
Mosinmiloluwa Moronkeji <sinmimoronkeji@Mosinmiloluwas-MacBook-Air.local>
2025-09-06 08:25:24 -0500

    hi

--- Lesson 1/rps.py
+++ Lesson 1/rps.py
@@ -1,13 +1,46 @@
 from tkinter import *
+import random
+
+C_score=0
+P_score=0
+
+choice=["rock","paper","siccors"]
+
+def play(pl_choice):
+    global C_score, P_score
+    cp_choice=random.choice(choice)
+    computer_choice.config(text=f"computer selected: {cp_choice}")
+    if pl_choice == cp_choice:
+        subtitle.config(text="Its a...Tie!",fg="grey")
+    elif pl_choice == "rock" and cp_choice == "siccors":
+        subtitle.config(text="Its a...WIN!",fg="goldenrod")
+        P_score+=1
+        player_score.config(text=f"Player Score: {P_score}")
+    elif pl_choice == "paper" and cp_choice == "rock":
+        subtitle.config(text="Its a...WIN!",fg="goldenrod")
+        P_score+=1
+        player_score.config(text=f"Player Score: {P_score}")
+    elif pl_choice == "siccors" and cp_choice == "paper":
+        subtitle.config(text="Its a...WIN!",fg="goldenrod")
+        P_score+=1
+        player_score.config(text=f"Player Score: {P_score}")
+    else:
+        subtitle.config(text="Its a...Loser womp womp :(",fg="red")
+        C_score+=1
+        computer_score.config(text=f"Computer Score: {C_score}")
+
 
 def rockcliked():
     player_choice.config(text="You Selected Rock")
+    play("rock")
 
 def papercliked():
     player_choice.config(text="You Selected Paper")
+    play("paper")
 
 def scissorscliked():
     player_choice.config(text="You Selected Sciccors")
+    play("siccors")
 
 window = Tk()
 window.geometry("800x400")
@@ -32,19 +65,19 @@ paper.grid(row=2, column=3, padx=10)
 scissors = Button(window, text="Scissors", width=10, height=2, bg="lightblue", command=scissorscliked)
 scissors.grid(row=2, column=4, padx=10)
 
-score_label = Label(window, text="Score", font=("Helvetica", 12), bg="white")
+score_label = Label(window, text="Score", font=("Helvetica", 12),fg="black",bg="white")
 score_label.grid(row=3, column=1, pady=30)
 
-player_choice = Label(window, text="You Selected:", font=("Helvetica", 12), bg="white")
+player_choice = Label(window, text="You Selected:", font=("Helvetica", 12),fg="black",bg="white")
 player_choice.grid(row=3, column=2)
 
-computer_choice = Label(window, text="Computer Selected:", font=("Helvetica", 12), bg="white")
+computer_choice = Label(window, text="Computer Selected:", font=("Helvetica", 12),fg="black",bg="white")
 computer_choice.grid(row=4, column=2)
 
-player_score = Label(window, text="Player Score:-", font=("Helvetica", 12), bg="white")
+player_score = Label(window, text="Player Score:-", font=("Helvetica", 12),fg="black",bg="white")
 player_score.grid(row=3, column=3, padx=20)
 
-computer_score = Label(window, text="Computer Score:-", font=("Helvetica", 12), bg="white")
+computer_score = Label(window, text="Computer Score:-", font=("Helvetica", 12),fg="black",bg="white")
 computer_score.grid(row=4, column=3, padx=20)
 
 window.mainloop()
