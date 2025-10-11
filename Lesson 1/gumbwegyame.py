7d1e35d4c1d00278cfed5f8073f2adaebfb0462f
CyberWarrior404 <cyberwarriorerrror404@gmail.com>
2025-10-11 08:17:30 -0500

    hi me

--- Lesson 1/gumbwegyame.py
+++ Lesson 1/gumbwegyame.py
@@ -1,7 +1,19 @@
 from tkinter import *
 import random
 
+words = ["python", "jumble", "random", "tkinter", "widget"]
+current_word=random.choice(words)
+def get_jumbled_word(word):
+    word_list = list(word)
+    word_label.config(text=random.shuffle(current_word))
+    
 
+def check_word():
+    user_input = entry.get().lower()
+    if user_input == current_word:
+        result_label.config(text="Correct!", fg="lightgreen")
+    else:
+        result_label.config(text=f"Wrong! It was '{current_word}'", fg="red")
 
 window = Tk()
 window.title("JUMBLED WORD GAME")
@@ -17,13 +29,14 @@ word_label.pack(pady=10)
 entry = Entry(window, font=("Arial", 14))
 entry.pack(pady=10, ipadx=20, ipady=5)
 
-check_button = Button(window, text="Check", bg="pink", fg="blue", font=("Arial", 12, "bold"))
+check_button = Button(window, text="Check", bg="pink", fg="blue", font=("Arial", 12, "bold"), command=check_word)
 check_button.pack(pady=5)
 
-reset_button = Button(window, text="Reset", bg="blue", fg="Pink", font=("Arial", 12, "bold"))
+reset_button = Button(window, text="Reset", bg="blue", fg="Pink", font=("Arial", 12, "bold"),command=get_jumbled_word)
 reset_button.pack(pady=5)
 
-result_label = Label(window, text="hi", font=("Arial", 14), bg="black")
+result_label = Label(window, text="", font=("Arial", 14), bg="black")
 result_label.pack(pady=10)
+
 
 window.mainloop()
